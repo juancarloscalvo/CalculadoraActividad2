552a0ac1248f2656a6929c092daac34436ca6676
Aida <aidareyesgalvan@hotmail.com>
2025-02-16 18:04:39 +0100

    ADD: Menú Suma y el test del cociente

--- src/main/java/javabeans/Main.java
+++ src/main/java/javabeans/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 
 public class Main {
-    static Scanner entrada = new Scanner(System.in);
+    public static Scanner entrada = new Scanner(System.in);
     static Cociente cociente = new Cociente();
 
     public static void main(String[] args) {
