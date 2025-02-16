36d3a15ffb9d280a25032a26ddbb9f4df05a6f92
Juan Carlos Calvo <juancarlos.calvo@ecam.es>
2025-02-16 17:18:23 +0100

    Add: URL Perfil git

--- src/main/java/javabeans/Cociente.java
+++ src/main/java/javabeans/Cociente.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * </ul>
  *
  * URL Github: https://github.com/juancarloscalvo/CalculadoraActividad2
+ * URL Perfil: https://github.com/juancarloscalvo
  * @author Juan Carlos Calvo Villardón
  * @version 0.2
  */
